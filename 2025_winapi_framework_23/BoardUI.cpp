85cc511d83a7bc9d41c339e57a5912613ddddc68
koori0831 <fuyunokoori0831@gmail.com>
2025-12-09 00:00:33 +0900

    feat : Omok UI

---
+++ 2025_winapi_framework_23/BoardUI.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.h"
+#include "BoardUI.h"
+
+void BoardUI::Update()
+{
+}
+
+void BoardUI::Render(HDC _hdc)
+{
+	wstring turnText;
+	if (m_gameState == GameState::PLAYING)
+	{
+		if (m_currentPlayer == StoneType::BLACK)
+			turnText = L"현재 턴: 흑";
+		else
+			turnText = L"현재 턴: 백";
+	}
+	else if (m_gameState == GameState::BLACK_WIN)
+		turnText = L"흑 승리!";
+	else if (m_gameState == GameState::WHITE_WIN)
+		turnText = L"백 승리!";
+
+	SetBkMode(_hdc, TRANSPARENT);
+	SetTextColor(_hdc, RGB(0, 0, 0));
+
+	RECT textRect;
+	textRect.left = 20;
+	textRect.top = 45;
+	textRect.right = textRect.left + 200;
+	textRect.bottom = textRect.top + 50;
+
+	DrawText(_hdc, turnText.c_str(), -1, &textRect, DT_LEFT | DT_TOP);
+	textRect.top += 50;
+	textRect.bottom += 50;
+
+	wstring resetText = L"R: 게임 재시작";
+	textRect.top += 30;
+	DrawText(_hdc, resetText.c_str(), -1, &textRect, DT_LEFT | DT_TOP);
+}
---
+++ 2025_winapi_framework_23/OmokTimer.cpp
@@ -0,0 +1,66 @@
+﻿#include "pch.h"
+#include "OmokTimer.h"
+
+OmokTimer::OmokTimer()
+	: m_elapsedTime(0.f)
+	, m_timeStopped(false)
+{
+	playerTime[0] = TIME_LIMIT; // 흑 시간 초기화
+	playerTime[1] = TIME_LIMIT; // 백 시간 초기화
+}
+
+OmokTimer::~OmokTimer()
+{
+}
+
+void OmokTimer::ResetTimer()
+{
+	m_elapsedTime = 0.f;
+	playerTime[0] = TIME_LIMIT; // 흑 시간 초기화
+	playerTime[1] = TIME_LIMIT; // 백 시간 초기화
+	m_timeStopped = false;
+	m_currentPlayer = StoneType::BLACK;
+}
+
+void OmokTimer::Update()
+{
+	if (!m_timeStopped && m_gameState == GameState::PLAYING)
+	{
+		TimeProcess();
+	}
+}
+
+void OmokTimer::Render(HDC _hdc)
+{
+	// 시간 표시
+	wstring blackTimeText =
+		std::format(L"흑 시간: {:02}:{:02}", static_cast<int>(playerTime[0]) / 60, static_cast<int>(playerTime[0]) % 60);
+	wstring whiteTimeText =
+		std::format(L"백 시간: {:02}:{:02}", static_cast<int>(playerTime[1]) / 60, static_cast<int>(playerTime[1]) % 60);
+	wstring timeText = blackTimeText + L"\n" + whiteTimeText;
+	SetBkMode(_hdc, TRANSPARENT);
+	SetTextColor(_hdc, RGB(0, 0, 0));
+	RECT textRect;
+	textRect.left = 20;
+	textRect.top = 10;
+	textRect.right = textRect.left + 200;
+	textRect.bottom = 60;
+	DrawText(_hdc, timeText.c_str(), -1, &textRect, DT_LEFT | DT_TOP);
+}
+
+void OmokTimer::TimeProcess()
+{
+	m_elapsedTime += fDT;
+	if (m_elapsedTime >= 1.f)
+	{
+		m_elapsedTime = 0.f;
+		if (m_currentPlayer == StoneType::BLACK)
+			playerTime[0] -= 1.f;
+		else
+			playerTime[1] -= 1.f;
+		if (playerTime[0] <= 0.f)
+			m_gameState = GameState::WHITE_WIN;
+		else if (playerTime[1] <= 0.f)
+			m_gameState = GameState::BLACK_WIN;
+	}
+}

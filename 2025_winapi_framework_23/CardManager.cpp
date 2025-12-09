f9796581ae4e16f5cdb0707f7749627a175f88f8
jiwon0612 <iololloli9976@gmail.com>
2025-12-09 17:37:48 +0900

    fix: CardManager

--- 2025_winapi_framework_23/CardManager.cpp
+++ 2025_winapi_framework_23/CardManager.cpp
@@ -126,8 +126,8 @@ void CardManager::CardDelete()
 
 void CardManager::RegisterCards()
 {
-	RegisterCard<IndiaInkCard>(L"먹물", L"상대의 화면에 먹물을 뿌린다", L"Bullet", CardRarity::Normal);
-	RegisterCard<EarthquakeCard>(L"지진", L"상대의 화면에 지진을 이르킨다", L"Bullet", CardRarity::Normal);
+	RegisterCard<IndiaInkCard>(L"먹물", L"상대의 화면에 먹물을 뿌린다", L"IndialnkIcon", CardRarity::Normal);
+	RegisterCard<EarthquakeCard>(L"지진", L"상대의 화면에 지진을 이르킨다", L"EarthquakeIcon", CardRarity::Normal);
 	RegisterCard<PlaceMineCard>(L"지뢰", L"지뢰를 설치한다 지뢰가 설치된 칸에는 돌을 둘수 없다", L"Bullet", CardRarity::Normal);
 	RegisterCard<TimeStopCard>(L"타이무스토뿌", L"시간을 멈춘다", L"Bullet", CardRarity::Normal);
 	RegisterCard<DeleteTimeCard>(L"시간 삭제", L"시간을 삭제한다", L"Bullet", CardRarity::Normal);
@@ -135,16 +135,16 @@ void CardManager::RegisterCards()
 	RegisterCard<RerollCard>(L"리롤", L"카드를 다시 뽑는다", L"Bullet", CardRarity::Normal);
 	RegisterCard<BlindAllStonesCard>(L"눈가리기", L"모든 돌을 블라인드 상태로 만든다", L"Bullet", CardRarity::Normal);
 	RegisterCard<OneMoreCard>(L"한번 더!", L"한번 더 돌을 놓을 수 있다", L"Bullet", CardRarity::Normal);
-	RegisterCard< PeaceCard>(L"평화의 시간", L"이번 턴 후 상대 턴과 내 턴 각 한 번 씩 카드가 생성되지 않는다.", L"Bullet", CardRarity::Normal);
-	RegisterCard< OneMinusCard>(L"카드 뺏기!", L"상대의 카드를 하나 뺏는다", L"Bullet", CardRarity::Normal);
-	RegisterCard< SoundChange>(L"음악 변경!", L"랜덤으로 음악을 변경한다.", L"Bullet", CardRarity::Normal);
-	RegisterCard< StopMusicCard>(L"고독한 시간", L"모든 음악을 멈춘다.", L"Bullet", CardRarity::Normal);
-	RegisterCard< ClapCard>(L"매우 축하!", L"박수소리를!", L"Bullet", CardRarity::Normal);
-	RegisterCard< DummyCard>(L"더미 카드!", L"놀랍게도 아무것도 안 합니다!", L"Bullet", CardRarity::Normal);
-	RegisterCard< SayHiCard>(L"바른 예의", L"안녕하세요!!!", L"Bullet", CardRarity::Normal);
-	RegisterCard< CheeringCard>(L"응원!", L"상대방에게 응원을 합니다.", L"Bullet", CardRarity::Normal);
-	RegisterCard< BlameCard>(L"비난", L"자신에게 비난을 합니다.", L"Bullet", CardRarity::Normal);
-	RegisterCard< RestCard>(L"휴식", L"게임을 초기화 합니다.", L"Bullet", CardRarity::Normal);
+	RegisterCard<PeaceCard>(L"평화의 시간", L"이번 턴 후 상대 턴과 내 턴 각 한 번 씩 카드가 생성되지 않는다.", L"Bullet", CardRarity::Normal);
+	RegisterCard<OneMinusCard>(L"카드 뺏기!", L"상대의 카드를 하나 뺏는다", L"Bullet", CardRarity::Normal);
+	RegisterCard<SoundChange>(L"음악 변경!", L"랜덤으로 음악을 변경한다.", L"Bullet", CardRarity::Normal);
+	RegisterCard<StopMusicCard>(L"고독한 시간", L"모든 음악을 멈춘다.", L"Bullet", CardRarity::Normal);
+	RegisterCard<ClapCard>(L"매우 축하!", L"박수소리를!", L"Bullet", CardRarity::Normal);
+	RegisterCard<DummyCard>(L"더미 카드!", L"놀랍게도 아무것도 안 합니다!", L"Bullet", CardRarity::Normal);
+	RegisterCard<SayHiCard>(L"바른 예의", L"안녕하세요!!!", L"Bullet", CardRarity::Normal);
+	RegisterCard<CheeringCard>(L"응원!", L"상대방에게 응원을 합니다.", L"Bullet", CardRarity::Normal);
+	RegisterCard<BlameCard>(L"비난", L"자신에게 비난을 합니다.", L"Bullet", CardRarity::Normal);
+	RegisterCard<RestCard>(L"휴식", L"게임을 초기화 합니다.", L"Bullet", CardRarity::Normal);
 }
 
 void CardManager::UnRegisterCard()

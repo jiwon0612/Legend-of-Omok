70d81cc2bd4f3c062e11d789600aca3c07eaa7b2
koori0831 <fuyunokoori0831@gmail.com>
2025-12-09 15:49:59 +0900

    feat : Spam Card part

--- 2025_winapi_framework_23/SpamCard.cpp
+++ 2025_winapi_framework_23/SpamCard.cpp
@@ -2,22 +2,53 @@
 #include "SpamCard.h"
 #include "SubWindow.h"
 #include "SceneManager.h"
+#include "ResourceManager.h"
+#include "WindowManager.h"
+#include "Texture.h"
+#include "SpamImage.h"
 
-SpamCard::SpamCard()
+SpamCard::SpamCard() : curPlayer(StoneType::NONE), spamUsed(false)
 {
 	isSkill = false;
+	spamUsed = false;
 }
 
 void SpamCard::Update()
 { 
 	if (!isSkill) return;
+    if (curPlayer != GET_SINGLE(BoardManager)->GetCurrentPlayer()) //턴 바뀜
+    {
+		for (int i = 0; i < 7; ++i)
+		{
+			wstring randTexKey = GET_SINGLE(ResourceManager)->GetSpamTexture();
+			Texture* spamTex = GET_SINGLE(ResourceManager)->GetTexture(randTexKey);
+			Vec2 screenSize = Vec2(GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN));
+			Vec2 randPos = Vec2((float)(rand() % (int)screenSize.x), (float)(rand() % (int)screenSize.y));
+			Vec2 textureSize = Vec2((float)spamTex->GetWidth(), (float)spamTex->GetHeight());
+			SubWindow* spamWindow = new SubWindow(GET_SINGLE(WindowManager)->GetHInstance(), {L"Spam"+i}, randPos, textureSize);
+
+			SpamImage* spamImage = new SpamImage(spamTex);
+			//spamWindow->AddObject(spamImage, Layer::BACKGROUND);
+		}
+		spamUsed = true;
+    }
+
+    if (!spamUsed) return;
+
+    if (curPlayer == GET_SINGLE(BoardManager)->GetCurrentPlayer()) //턴 바뀜
+    {
+        NextTurn();
+    }
 }
 
 void SpamCard::CardSkill()
 {
+	curPlayer = GET_SINGLE(BoardManager)->GetCurrentPlayer();
 	isSkill = true;
 }
 
 void SpamCard::NextTurn()
 {
+	isSkill = false;
+	spamUsed = false;
 }
---
+++ 2025_winapi_framework_23/SpamImage.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "SpamImage.h"
+#include "Texture.h"
+
+SpamImage::SpamImage(Texture* tex)
+{
+	m_texture = tex;
+}
+
+void SpamImage::Update()
+{
+}
+
+void SpamImage::Render(HDC _hdc)
+{
+	BitBlt(_hdc,
+		0,
+		0,
+		m_texture->GetWidth(),
+		m_texture->GetHeight(),
+		m_texture->GetTextureDC(),
+		0,
+		0,
+		SRCCOPY);
+}

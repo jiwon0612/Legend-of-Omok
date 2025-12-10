c6151583fc6b7c134cfd62c7131645df963a60bd
koori0831 <fuyunokoori0831@gmail.com>
2025-12-10 12:14:35 +0900

    feat : spam window use random seed

--- 2025_winapi_framework_23/SpamCard.cpp
+++ 2025_winapi_framework_23/SpamCard.cpp
@@ -8,6 +8,7 @@
 #include "SpamImage.h"
 #include "SpamWindow.h"
 #include "SpamCloseButton.h"
+#include <chrono>
 
 SpamCard::SpamCard() : curPlayer(StoneType::NONE), spamUsed(false)
 {
@@ -23,12 +24,12 @@ void SpamCard::Update()
 		{
 			wstring randTexKey = GET_SINGLE(ResourceManager)->GetSpamTexture();
 			Texture* spamTex = GET_SINGLE(ResourceManager)->GetTexture(randTexKey);
-			Vec2 screenSize = Vec2(GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN));
+			Vec2 screenSize = Vec2(GetSystemMetrics(SM_CXSCREEN) - 400, GetSystemMetrics(SM_CYSCREEN) - 400);
 			Vec2 randPos = Vec2((float)(rand() % (int)screenSize.x), (float)(rand() % (int)screenSize.y));
 			Vec2 textureSize = Vec2((float)spamTex->GetWidth(), (float)spamTex->GetHeight());
 
-			// 안전한 윈도우 키 생성: L"Spam" + i 형태의 포인터 산술 제거
-			std::wstring spamKey = std::wstring(L"Spam") + std::to_wstring(i);
+			auto nowStamp = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+			std::wstring spamKey = std::wstring(L"Spam") + std::to_wstring(nowStamp + i);
 
 			SpamWindow* spamWindow = new SpamWindow(GET_SINGLE(WindowManager)->GetHInstance(), spamKey, randPos, textureSize);
 			GET_SINGLE(WindowManager)->AddWindow(spamWindow);
@@ -37,7 +38,7 @@ void SpamCard::Update()
 			spamWindow->AddObject(spamImage);
 			GET_SINGLE(SceneManager)->GetCurScene()->RequestSpawn(spamImage, Layer::UI);
 			spamImage->SetWindowType(spamKey);
-			SpamCloseButton* closeButton = new SpamCloseButton(spamKey, { textureSize.x - 30.f, 0.f }, { 30.f,30.f });
+			SpamCloseButton* closeButton = new SpamCloseButton(spamKey, { textureSize.x - 30.f, 20.f }, { 30.f,30.f });
 			spamWindow->AddObject(closeButton);
 			GET_SINGLE(SceneManager)->GetCurScene()->RequestSpawn(closeButton, Layer::BUTTON);
 			closeButton->SetWindowType(spamKey);
